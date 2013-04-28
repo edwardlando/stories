8c12d055cd9aa4f5dac5af8cc9803f93a414cbbf
edwardlando <edwardlando8@gmail.com>
2013-04-27 23:24:59 -0400

    stories first commit

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+  attr_accessible :content, :title
+
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :content, presence: true
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stories::Application.config.secret_token = '246560eb82b38d4a56b71274ae10928e93b8a19cad2e46aacfbbbbf9f21f549ddc388625ea1c4385400c83e98b0725a4e14927053f050de168647fa66483cd95'
